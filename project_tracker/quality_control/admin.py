e3947d93a7b067334bf41a53033cb88d35d76015
Depthmane <depthmane@yandex.ru>
2024-04-12 20:28:53 +0300

    added BugReport and FeatureRequest to adminpanel

--- project_tracker/quality_control/admin.py
+++ project_tracker/quality_control/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import FeatureRequest, BugReport
 
-# Register your models here.
+
+# класс администратора для BugReport
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'task')
+    list_filter = ('status', 'priority', 'project')
+    search_fields = ('name', 'description')
+    ordering = ('created_at',)
+    date_hierarchy = 'created_at'
+
+
+# класс администратора для FeatureRequest
+
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'status', 'created_at', 'updated_at')
+    list_filter = ('status', 'task', 'project',)
+    search_fields = ('name', 'description')
+    readonly_fields = ('created_at', 'updated_at')
--- project_tracker/tasks/urls.py
+++ project_tracker/tasks/urls.py
@@ -3,7 +3,7 @@ from tasks import views
 
 app_name = 'tasks'
 
-urlpatterns = [
+urlpatterns = [ # функциональные представления
 #   path('', views.index),
 #   path('projects/', views.projects_list, name='projects_list'),
 #   path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
@@ -14,9 +14,8 @@ urlpatterns = [
 #   path('projects/<int:project_id>/update', views.update_project, name='update_project'),
 #   path('projects/<int:project_id>/delete/', views.delete_project, name='delete_project'),
 #   path('projects/<int:project_id>/tasks/<int:task_id>/delete', views.delete_task, name='delete_task'),
-# функциональные представления
 
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.IndexView.as_view(), name='index'), # классовые представления
 
     path('project/create/', views.ProjectCreateView.as_view(), name='create_project'),
     path('projects/', views.ProjectsListView.as_view(), name='projects_list'),
@@ -30,5 +29,4 @@ urlpatterns = [
 
     path('projects/<int:project_id>/delete/', views.ProjectDeleteView.as_view(), name='delete_project'),
     path('projects/<int:project_id>/tasks/<int:task_id>/delete/', views.TaskDeleteView.as_view(), name='delete_task'),
-# классовые представления
 ]
